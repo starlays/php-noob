1d66ef3c414b4e10f66439bbabe8f270ce7568f0
Lazar Florentin <florentin.lazar@gmail.com>
2013-09-17 14:31:36 +0300

    update exercise 1.php

--- exercises/exercise 1.php
+++ exercises/exercise 1.php
@@ -10,54 +10,63 @@
 interface Parkable {
     /**
      * setWidth (str $width) method used to set parkable widht
-     * 
+     *
      * @param int $width the width of parkable object
+     *
      * @return null
      */
     public function setWidth($width);
     /**
      * setHeight (str $height) method used to set parkable height
-     * 
+     *
      * @param int $height the height of parkable object
+     *
      * @return null
      */
     public function setHeight($height);
     /**
      * setLenght (str $length) method used to set parkable length
-     * 
+     *
      * @param int $length the length of parkable object
+     *
      * @return null
      */
     public function setLenght($length);
     /**
      * setIsParked (str $isParked) method used to set parkable parked state
-     * 
+     *
      * @param bool $isParked the parked state of the parkable object
+     *
      * @return null
      */
     public function setIsParked($isParked);
     /**
      * getWidth() method used to get the width
+     *
      * @return null
      */
     public function getWidth();
     /**
      * getHeight() method used to get the height
+     *
      * @return null
      */
     public function getHeight();
     /**
      * getLength() method used to get the length
+     *
      * @return null
      */
     public function getLength();
     /**
      * getSurface() method used to get the area of the parkable object
+     *
      * @return null
      */
     public function getSurface();
     /**
      * getParkState() method used to get parkable state (parked or nod)
+     *
      * @return null
      */
     public function getParkState();
@@ -68,26 +77,55 @@ interface Parkable {
 interface Parkplace {
     /**
      * setTotalSurface(int $totalSurface) method used to set parkplace surface
-     * 
+     *
      * @param int $totalSurface
+     *
+     * @throws Exception
      * @return null
      */
     public function setTotalSurface($totalSurface);
      /**
      * setAllowDim(array $allowDim) method used to set the maxium dimensions
      * allowd in to the parkplace
-     * 
-     * @param type $allowDim
-     * @return null 
+     *
+     * @param array $allowDim
+      *
+     * @throws Exception
+     * @return null
      */
     public function setAllowDim($allowDim = array());
     /**
-     * getAvailableSurface() method used to get the availale surface left in
-     * the parkplace
-     * 
+     * setTotalParkble(int $totalParkble) method use to set the total pakble
+     *
+     * @param int $totalParkble
+     *
+     * @throws Exception
      * @return null
      */
+     public function setTotalParkble($totalParkble);
+     /**
+      * setParkable(Parkable $parkable) set Parkable parking state
+      *
+      * @param Parkable $parkable
+      *
+      * @throws Exception
+      * @return bool
+      */
+     public function setParkable(Parkable $parkable);
+    /**
+     * getAvailableSurface() method used to get the availale surface left in
+     * the parkplace
+     *
+     * @return availableSurface
+     */
     public function getAvailableSurface();
+    /**
+     * getTotalParkble() used to get the total parkable
+     *
+     * @return totalParkble
+     */
+    public function getTotalParkble();
+
 }
 
 class garage implements Parkplace
@@ -99,7 +137,7 @@ class garage implements Parkplace
     /**
      * @var int totalCars holder of the total parked cars
      */
-    protected $totalCars    = 0;
+    protected $totalParkble    = 0;
     /**
      * @var mixed $colors how many cars of eatch colors are parked in the
      * garage
@@ -110,133 +148,159 @@ class garage implements Parkplace
      * that will be parked in the garage
      */
     protected $allowDim     = array(
-                                'smin' =>3,
                                 'smax' =>5,
                                 'hmax' =>2
                              );
-
     /**
-     * 
-     * @param string $surface the total available area of the garage
-     * @param array $cars the number of pre parked cars, empty by default
-     * @param array $allowDim(
-     *                      'smin' => <value>, min area, def 3m^2
-     *                      'smax' => <value>, max area, def 5m^2
-     *                      'hmax' => <value>  max car height, def 2m
-     *                      )
-     * @return null
+     * Error messages
      */
-    
-    
-    public function __construct($cars = array(), $allowDim = array())
+    const ERR_SURFACE  = 'Parameter $surface must be type string';
+    const ERR_ALLOWDIM = 'Parameter $allowDim array geometry is not according to signature';
+    const ERR_FORMAT   = 'Parameter $allowDim is not array or empty';
+    const ERR_SMAX     = 'Maximum area exceeded';
+    const ERR_HMAX     = 'Maximum height exceeded';
+    const ERR_PARKBLE  = 'Parameter $totalParkble must be int and not 0';
+
+    public function setParkable(Parkable $parkable)
     {
-
-        if(!is_array($cars)) {
-            throw new Exception('Parameter $cars is not array');
+        if($parkable->surface >= $this->allowDim['smax']) {
+            throw new Exception(ERR_SMAX, 4);
         }
-        if(!is_array($allowDim) || empty($allowDim)) {
-            throw new Exception('Parameter $allowDim is not array or empty');
-        }
-        if(!isset($allowDim['smin']) && !isset($allowDim['smax']) &&
-                                                !isset($allowDim['hmax'])) {
-            throw new Exception('Parameter $allowDim array geometry is not
-            according to signature');
-        }
-
-       $this->totalSurface = $surface;
-
-        if(!empty($cars)) {
-            $this->totalCars = count($cars);
-        }
-
-        $this->allowDim = $allowDim;
-    }
-    /**
-     * setParkCar method used to park a car in the garage
-     * @param int $carSurface the area of a parked car
-     * @param str $carColor the color of the car that is going to pe parked
-     * @return bool if the car was parked or not
-     */
-    public function setParkCar(Icar $car)
-    {
-        if( $car->surface >= $this->allowDim['smin'] &&
-            $car->surface <= $this->allowDim['smax'] &&
-            $car->height <= $this->allowDim['hmax'] )
+        if($parkable->height >= $this->allowDim['hmax'])
         {
-            $this->totalSurface = $this->totalSurface - $car->surface;
-            !in_array($car->color, $this->colors) ?
-                  $this->colors[$car->color] = 1 : ++$this->colors[$car->color];
-            return TRUE;
+            throw new Exception(ERR_HMAX, 5);
         }
-        return FALSE;
+
+        $this->totalSurface = $this->totalSurface - $parkable->surface;
+        $parkable->setIsParked(TRUE);
+
+        return TRUE;
     }
-    /**
-     * @throws Exception
-     */
+
     public function setTotalSurface($totalSurface)
     {
         if (!is_int($totalSurface)) {
-            throw new Exception('Parameter $surface must be type string');
+            throw new Exception(ERR_SURFACE, 1);
         }
+        $this->totalSurface = $totalSurface;
+    }
 
+    public function setAllowDim($allowDim = array())
+    {
+        if(!is_array($allowDim) || empty($allowDim)) {
+            throw new Exception(ERR_FORMAT, 2);
+        }
+        if(!isset($allowDim['smin']) && !isset($allowDim['smax']) &&
+                                                !isset($allowDim['hmax'])) {
+            throw new Exception(ERR_ALLOWDIM, 3);
+        }
+        $this->allowDim = $allowDim;
     }
-    public function setAllowDim($allowDim = array()) {
-        
+
+    public function setTotalParkble($totalParkble)
+    {
+        if(!is_int($totalParkble) || 0 == $totalParkble) {
+            throw new Exception(ERR_PARKBLE, 4);
+        }
     }
-    public function getAvailableSurface() {
-        
+
+    public function getAvailableSurface()
+    {
+        return $this->totalSurface;
+    }
+
+    public function getTotalParkble()
+    {
+        return $this->totalParkble;
     }
 }
 
-class car implements Parkplace
+class car implements Parkable
 {
-    /**
-     * @var int $surface the area that a car occupies
+     /**
+     * @var int $width the width of the car
      */
-    private $surface = 0;
+    protected $width  = 0;
+     /**
+     * @var int $lenght the height of the car
+     */
+    protected $lenght  = 0;
     /**
      * @var int $height the height of the car
      */
-    private $height  = 0;
+    protected $height  = 0;
     /**
      * @var string $color the color of the car
      */
-    private $color   = NULL;
+    protected $color   = NULL;
     /**
-     * Class constructor used to initialize an car object
-     * @param int $carSurface the area that the parked will take
-     * @param int $carHeight the heigth of the car
-     * @param str $carColor the color of the car that will be parked
-     * @retunr NULL
+     * @var bool $isParked car status holder, is parked or not
      */
-    public function __construct($carSurface, $carHeight, $carColor)
-    {
-        if(!is_integer($carSurface) || $carSurface < 0) {
-            throw new Exception('Parameter $carSurface must be type integer
-            and greater then 0');
+    protected $isParked = FALSE;
+    /**
+     * Error constants
+     */
+    const ERR_WIDTH  = 'Parameter $width must be int possitiv different from 0';
+    const ERR_LENGHT = 'Parameter $lenght must be int possitiv diferent from 0';
+    const ERR_HEIGHT = 'Parameter $height must be int possitiv diferent from 0';
+    const ERR_PARKED = 'Parameter $isParked must be boolean';
+    
+    public function setWidth($width) {
+        if(!is_int($width) || 0 == $width) {
+            throw new Exception(ERR_WIDTH, 6);
         }
-        if(!is_string($carColor)) {
-            throw new Exception('Parameter $carColor must be type string');
-        }
-        if(!is_integer($carHeight) || $carHeight < 0) {
-            throw new Exception('Parameter $carH must be integer and greater
-            then 0');
-        }
-        $this->height  = $carHeight;
-        $this->surface = $carSurface;
-        $this->color   = $carColor;
+        
+        $this->width = $width;
     }
-    // class getters
-    public function getSurface()
-    {
-        return $this->surface;
+
+    public function setLenght($length) {
+       if(!is_int($length) || 0 == $length) {
+            throw new Exception(ERR_LENGHT, 7);
+        }
+        
+        $this->length = $length;
     }
+    
+    public function setHeight($height) {
+       if(!is_int($height) || 0 == $height) {
+            throw new Exception(ERR_HEIGHT, 8);
+        }
+        
+        $this->height = $height;
+    }
+    
+    public function setIsParked($isParked) {
+       if(!is_bool($isParked) || empty($isParked)) {
+            throw new Exception(ERR_PARKED, 9);
+        }
+        
+        $this->isParked = $isParked;
+    }
+
+    
+    public function getWidth() {
+        return $this->width;
+    }
+    
+    public function getSurface() {
+        return $this->width * $this->length;
+    }
+    
+    public function getParkState() {
+        return $this->isParked;
+    }
+    
+    public function getLength() {
+        return $this->length;
+
+    }
+    
+    public function getHeight() {
+        return $this->height;
+    }
+    
     public function getColor()
     {
         return $this->color;
-    }
-    public function getHeight()
-    {
-        return $this->height;
     }
 }
