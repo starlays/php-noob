3c19c69c1b33ec549fd42a7b412783c9b1b71850
Lazar Florentin <florentin.lazar@gmail.com>
2013-10-09 16:28:39 +0300

    Update exercise 1.php

--- exercises/exercise 1.php
+++ exercises/exercise 1.php
@@ -13,24 +13,29 @@ interface IMeasurable
 
 interface IParkplace extends IMeasurable
 {
-    public function checkMesures(IParkble $parkable);
-    public function addParkable(IParkble $parkable);
-    public function parkOut();
+    public function addParkable(IParkable $parkable);
+    public function leaveParkplace(IParkable $parkable);
 }
 
 interface IParkable extends IMeasurable
 {
     public function parkIn(IParkplace $parkPlace);
+    public function parkOut();
+    public function isParked();
 }
 
-abstract class ParcableBase implements IParkable
+abstract class ParkableBase implements IParkable
 {
     protected $height = 0;
     protected $length = 0;
     protected $width  = 0;
+    protected $parkPlace = NULL;
 
     public function setHeight($height)
     {
+        if($this->height) {
+            throw new Exception('Height already set', 1);
+        }
         $this->height = $height;
         return $this;
     }
@@ -42,6 +47,9 @@ abstract class ParcableBase implements IParkable
 
     public function setLength($length)
     {
+        if($this->length) {
+            throw new Exception('Length already set', 2);
+        }
         $this->length = $length;
         return $this;
     }
@@ -53,6 +61,9 @@ abstract class ParcableBase implements IParkable
 
     public function setWidth($width)
     {
+        if($this->width) {
+            throw new Exception('Width already set', 3);
+        }
         $this->width = $width;
         return $this;
     }
@@ -64,18 +75,42 @@ abstract class ParcableBase implements IParkable
 
     public function parkIn(IParkplace $parkPlace)
     {
+        if($this->isParked()) {
+            throw new Exception('Parkable already parked', 4);
+        }
         $parkPlace->addParkable($this);
+        $this->parkPlace = $parkPlace;
+        return $this;
+    }
+
+    public function parkOut()
+    {
+        if(!$this->isParked()) {
+            throw new Exception('Parkable is not parked', 5);    
+        }
+        $this->parkPlace->leaveParkplace($this);
+        $this->parkPlace = NULL;
+        return $this;
+    }
+
+    public function isParked()
+    {
+        return NULL != $this->parkPlace;
     }
 }
 
-abstract class ParkableBase implements IParkplace
+abstract class ParkplaceBase implements IParkplace
 {
     protected $height = 0;
     protected $length = 0;
     protected $width  = 0;
-    
+    protected $parkedParkables = array();
+
     public function setHeight($height)
     {
+        if($this->height) {
+            throw new Exception('Height already set', 6);
+        }
         $this->height = $height;
         return $this;
     }
@@ -87,6 +122,9 @@ abstract class ParkableBase implements IParkplace
 
     public function setLength($length)
     {
+        if($this->length) {
+            throw new Exception('Length already set', 7);
+        }
         $this->length = $length;
         return $this;
     }
@@ -98,6 +136,9 @@ abstract class ParkableBase implements IParkplace
 
     public function setWidth($width)
     {
+        if($this->width) {
+            throw new Exception('Width already set', 8);
+        }
         $this->width = $width;
         return $this;
     }
@@ -106,21 +147,84 @@ abstract class ParkableBase implements IParkplace
     {
         return $this->width;
     }
-    
-    public function checkMesures(IParkble $parkable)
+
+    public function addParkable(IParkable $parkable)
     {
         if($parkable->getHeight() >= $this->height) {
-            
+            throw new LengthException("Height is to big", 9);
         }
-    }
-    
-    public function addParkable(IParkble $parkable)
-    {
+        if($parkable->getWidth() >= $this->width){
+            throw new LengthException("Width is to big", 10); 
+        }
+        if($parkable->getLength() >= $this->length){
+            throw new LengthException("Length is to big", 11); 
+        }
+        if($parkable->isParked()){
+            throw new Exception("Parkable already parked", 12); 
+        }
+        // add the parkable to the garage
+        $this->parkedParkables[] = $parkable;
         
+        return TRUE;
     }
-    public function parkOut()
+
+    public function leaveParkplace(IParkable $parkable)
     {
-        
-    }    
+        // get the parkable out from the garage
+        if(($key = array_search($parkable, $this->parkedParkables)) !== FALSE) {
+            unset($this->parkedParkables[$key]);
+        }
+        // set the parkable parking state
+        $parkable->parkState = FALSE; 
+    }
 }
 
+class car extends ParkableBase {
+}
+
+class garage extends ParkplaceBase {
+}
+
+// ----- usage ----------------------
+
+try {
+    $carA = new car();
+    $carA->setHeight(95)->setWidth(150)->setLength(190);
+
+    $carB = new car();
+    $carB->setHeight(95)->setWidth(150)->setLength(190);
+
+    $garageA = new garage();
+    $garageA->setHeight(120)->setWidth(200)->setLength(230);
+
+    $garageB = new garage();
+    $garageB->setHeight(120)->setWidth(200)->setLength(230);
+
+    // $carA->parkOut(); // a car that is not parked can't be parked out
+
+    // the car asks for parking place
+    $carA->parkIn($garageA);
+
+    //  $carA->parkIn($garageA); // a car can't be parked twice in the same garage
+
+    // $carA->setWidth(1000); // car already initialized
+    
+    // $carA->parkin($garageB); // car can't change park place if it's parked
+    
+    // $garageA->setHeight(9999); // garage already initialized
+
+    $carB->parkIn($garageA);
+    // the car leaves the parkplace
+    $carB->parkOut();
+    
+    // $carB->parkOut(); // a car can't be unparked more then once
+
+    var_dump($carA, $garageA);
+
+    // the garageA sends the car out from the parking place
+    $garageA->leaveParkplace($carA);
+    var_dump($carA,$carB, $garageA);
+    $carB->parkIn($garageA);
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), ' error code: #', $e->getCode(), "\n";
+}
